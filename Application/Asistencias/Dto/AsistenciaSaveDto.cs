fb3ec9b3886bc7b4fe4e26fe41a55cd1961ca484
jhonLapa <cotos.jhon@gmail.com>
2025-10-08 17:17:19 -0500

    MEJORAS ASISTENCIA

--- Application/Asistencias/Dto/AsistenciaSaveDto.cs
+++ Application/Asistencias/Dto/AsistenciaSaveDto.cs
@@ -13,7 +13,6 @@ namespace Application.Asistencias.Dto
         public string Tipo { get; set; }
         public int HorasTrabajadas { get; set; }
         public string? Observacion { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public string UsuarioCreacion { get; set; }
+        public string? UsuarioCreacion { get; set; }
     }
 }
