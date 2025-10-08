7e2921bc493ae3efec2369e295475983276aa2eb
DevRonaldino <ronaldinoch30@gmail.com>
2025-10-08 16:53:58 -0500

    asistencias commit

---
+++ Application/Asistencias/Dto/Profiles/AsistenciaProfile.cs
@@ -0,0 +1,19 @@
+﻿using Application.Asistencias.Dto;
+using AutoMapper;
+using Domain;
+
+namespace Application.Asistencias.Dtos.Profiles
+{
+    public class AsistenciaProfile : Profile
+    {
+        public AsistenciaProfile()
+        {
+            // Asistencia básica
+            CreateMap<Asistencia, AsistenciaDto>().ReverseMap();
+            CreateMap<Asistencia, AsistenciaSaveDto>().ReverseMap();
+            CreateMap<Asistencia, AsistenciaSelectDto>().ReverseMap();
+
+
+        }
+    }
+}
---
+++ Infraestructure/Repositories/Interfaces/IAsistenciaRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+using Infraestructure.Core.Repositories.Interfaces;
+
+namespace Infraestructure.Repositories.Interfaces
+{
+    public interface IAsistenciaRepositorio : ICrudCoreRespository<Asistencia, int>
+    {
+        Task<PaginadoResponse<Asistencia>> BusquedaPaginado(PaginationRequest dto);
+        Task<IReadOnlyList<Asistencia>> SelectActivo();
+    }
+}
